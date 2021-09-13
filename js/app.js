a9a3737a6c7dcf53767f09550d38c9f2170eabf1
Zach Stone <zachinjapan888s@gmail.com>
2021-09-13 17:05:41 -0400

    created lis and added the text for each section

--- js/app.js
+++ js/app.js
@@ -19,14 +19,36 @@
  */
 const header = document.querySelector(".page__header");
 const nav = document.querySelector(".navbar__menu");
-const navList = document.querySelector(".navbar__list");
+const navList = document.querySelector("#navbar__list");
 const sections = document.querySelectorAll('section');
+const sectionCount = sections.length;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  */
 
+// create an array of the documents sections to use as the limit for the functions
+const SectionArray = [];
+
+// function NavArray(numberOfSections) {
+
+
+//     for (x = 1; x !== numberOfSections + 1; x++) {
+
+//         SectionArray.push("Section " + x);
+//     };
+// };
+
+// create a bunch of li and add the html section + # to it
+function makeLi(numberOfSections) {
+    for (x = 1; x !== numberOfSections + 1; x++) {
+        let navItem = document.createElement("li");
+        navItem.textContent = "Section " + x;
+        navList.appendChild(navItem);
+
+    }
+}
 
 
 
@@ -36,18 +58,15 @@ const sections = document.querySelectorAll('section');
  * 
  */
 
-// console.log(sections.childElementCount);
+// create nav bar
 
-function buildNav(sectionCount) {
 
-    for (x = 1; x !== sectionCount + 1; x++) {
-        console.log("section " + x);
-    };
-};
 
 // build the nav
 
-buildNav(sections.length);
+// NavArray(sectionCount);
+makeLi(sectionCount);
+
 
 
 // Add class 'active' to section when near top of viewport

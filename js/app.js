c580ee96502bed652f896440770d699d212de160
Zach Stone <zachinjapan888s@gmail.com>
2021-09-14 17:59:09 -0400

    add event listeners to all sections for mouseenter and mouseleave

--- js/app.js
+++ js/app.js
@@ -17,38 +17,11 @@
  * Define Global Variables
  * 
  */
-const header = document.querySelector(".page__header");
-const nav = document.querySelector(".navbar__menu");
-const navList = document.querySelector("#navbar__list");
-const sections = document.querySelectorAll('section');
-const sectionCount = sections.length;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  */
-
-// create an array of the documents sections to use as the limit for the functions
-const SectionArray = [];
-
-// function NavArray(numberOfSections) {
-
-
-//     for (x = 1; x !== numberOfSections + 1; x++) {
-
-//         SectionArray.push("Section " + x);
-//     };
-// };
-
-// create a bunch of li and add the html section + # to it
-function makeLi(numberOfSections) {
-    for (x = 1; x !== numberOfSections + 1; x++) {
-        let navItem = document.createElement("li");
-        navItem.textContent = "Section " + x;
-        navList.appendChild(navItem);
-
-    }
-}
 
 
 
@@ -64,8 +37,6 @@ function makeLi(numberOfSections) {
 
 // build the nav
 
-// NavArray(sectionCount);
-makeLi(sectionCount);
 
 
 
